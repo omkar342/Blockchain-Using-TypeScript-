21f1af8dd412cc2a67bca3fad058ffe9cdfe09ac
Omkar Jadhav <omkar.jadhav@cerence.com>
2023-04-15 23:20:28 +0530

    installed ethereum-cryptography npm package and created a hashGenerator function

--- blockchainDemonstration.ts
+++ blockchainDemonstration.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import { sha256 } from "ethereum-cryptography/sha256";
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
+class BlockcahinDemonstration {
+  data: string;
+  hash: string;
+  prev_hash: string;
+
+  constructor(data: string, hash: string, prev_hash: string) {
+    this.data = data;
+    this.hash = hash;
+    this.prev_hash = prev_hash;
+  }
+}
+
+const hashGenerator = (data: string): string => {
+  const stringToUTF8: Uint8Array = utf8ToBytes(data);
+  const hash: Uint8Array = sha256(stringToUTF8);
+  const hexaDecimalHash: string = toHex(hash);
+  return hexaDecimalHash;
+};
+
+const dataInHexaDecimalHash = hashGenerator("Hello, world!");
+
+console.log(dataInHexaDecimalHash);
